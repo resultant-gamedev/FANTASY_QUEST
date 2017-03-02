814625175a04f97e2bd7cfca4d934aedc4580ca3
Jahn Johansen <0x6a61686e@gmail.com>
2017-03-02 00:24:48 -0500

    Removed Prop base class. Just going to use KinematicBody2D, RigidBody2D is a headache.

--- scenes/entities/Prop.gd
+++
@@ -1,9 +0,0 @@
-# Base class for all props. Pots, switches, etc.
-# FIXME: Gravity is applied when set to Rigid mode. Won't turn the fuck off. Could just avoid Rigid, but why not just use Kinematic then?
-
-extends RigidBody2D
-
-# TODO: Design some example props.
-
-func _ready():
-	pass
